89622af20fefe6c793d530620b4ab9f5ddb9526b
Xewol <bartekdeluga0702@gmail.com>
2023-07-05 10:49:49 +0200

    feat: dashboard

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex-1 max-w-[88rem] mx-auto m-10 shadow-2xl rounded-xl flex p-2">
+      <div className="basis-1/5  border-r border-gray-800"></div>
+      <div className="basis-full "></div>
+    </div>
+  );
+};
+
+export default Page;
