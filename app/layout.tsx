fb684972d64f10d226b1cec8370860fd7e35d1f6
Xewol <bartekdeluga0702@gmail.com>
2023-07-21 09:00:36 +0200

    remove: font awesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
 import "./globals.css";
 import { Roboto } from "next/font/google";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
 
 const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
